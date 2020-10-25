4c58fc4d497e0bd747e006d31691fb0708760780
mathiasjespers <mathias.jespers@student.ehb.be>
2020-10-25 18:51:55 +0100

    layout decryptor.js

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -8,9 +8,7 @@ module.exports = async(key) => {
     return Object.freeze({
         decrypt: (ciphertext, nonce) => {
             (async() => await nacl.ready)();
-
             if(!ciphertext || !nonce) throw 'Invalid arguments';
-
             return nacl.crypto_secretbox_open_easy(ciphertext, nonce, key)
         }
     });
