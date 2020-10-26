15e926b8bbf59f6fc10fb62885fad343af6a8ff1
mathiasjespers <mathias.jespers@student.ehb.be>
2020-10-26 15:31:58 +0100

    signaturejs

--- src/Signature.js
+++ src/Signature.js
@@ -1,3 +1,15 @@
-module.exports = () => {
-    
+const nacl = require('libsodium-wrappers');
+
+module.exports = async() => {
+    await nacl.ready;
+
+    let keyPair = nacl.crypto_sign_keypair();
+    const [pk, sk] = [keyPair.publicKey, keyPair.privateKey];
+
+    return Object.freeze({
+        verifyingKey: pk,
+        sign: msg => {
+            return nacl.crypto_sign(msg, sk);
+        }
+    });
 }
