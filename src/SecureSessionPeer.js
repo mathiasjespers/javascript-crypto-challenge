9bfc696f77ebb555aac711b5bc4333f058901991
mathiasjespers <mathias.jespers@student.ehb.be>
2020-10-26 20:32:42 +0100

    securesession object

---
+++ src/SecureSessionPeer.js
@@ -0,0 +1,31 @@
+const nacl = require('libsodium-wrappers');
+const Decryptor = require('./Decryptor.js')
+const Encryptor = require('./Encryptor.js')
+
+module.exports = async (peer) => {
+
+    await nacl.ready;
+
+    let otherPeer = peer;
+    let encryptor, decryptor, rx, tx;
+    let keyPair = nacl.crypto_kx_keypair()
+    let [pk, sk] = [keyPair.publicKey, keyPair.privateKey]
+
+    global.message;
+
+    Object.freeze(peerInstance)
+
+    if(otherPeer !== undefined){
+        let serverKeyPair = nacl.crypto_kx_server_session_keys(pk, sk, otherPeer.publicKey)
+        rx = serverKeyPair.sharedRx;
+        tx = serverKeyPair.sharedTx;
+
+        otherPeer.generateSharedKeys(peerInstance);
+        decryptor = await Decryptor(rx)
+        encryptor = await Encryptor(tx)
+    }
+
+
+    return peerInstance;
+
+}
